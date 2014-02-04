97b89a549d60a0cdcbfe325515d258d1331557a6
James Lupiani <lupiani@sickheadgames.com>
2014-02-04 16:05:36 -0600

    New setup.

---
+++ Compare/BuildWithMonoGame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare
+{
+    extern alias monogame;
+    using monogame::Microsoft.Xna.Framework.Content.Pipeline;
+
+    class MonoGameImporterContext : ContentImporterContext
+    {
+        private readonly string _intermediateDir;
+        private readonly string _outputDir;
+        private readonly ContentBuildLogger _logger;
+        private readonly List<string> _dependencies = new List<string>();
+
+        public MonoGameImporterContext(string intermediateDir, string outputDir)
+        {
+            _intermediateDir = intermediateDir;
+            _outputDir = outputDir;
+            _logger = new MonoGameConsoleLogger();
+        }
+
+        public override void AddDependency(string filename)
+        {
+            _dependencies.Add(filename);
+        }
+
+        public override string IntermediateDirectory { get { return _intermediateDir; } }
+        public override ContentBuildLogger Logger { get { return _logger; } }
+        public override string OutputDirectory { get { return _outputDir; } }
+    }
+
+    public class MonoGameConsoleLogger : ContentBuildLogger
+    {
+        public override void LogMessage(string message, params object[] messageArgs)
+        {
+            Console.WriteLine(message, messageArgs);
+        }
+
+        public override void LogImportantMessage(string message, params object[] messageArgs)
+        {
+            Console.WriteLine(message, messageArgs);
+        }
+
+        public override void LogWarning(string helpLink, ContentIdentity contentIdentity, string message, params object[] messageArgs)
+        {
+            var msg = string.Format(message, messageArgs);
+            var fileName = GetCurrentFilename(contentIdentity);
+            Console.WriteLine("{0}: {1}", fileName, msg);
+        }
+    }
+}
---
+++ Compare/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KellermanSoftware.CompareNetObjects;
+using System.IO;
+
+namespace Compare
+{
+    extern alias xna;
+    extern alias monogame;
+
+    using XnaFbxImporter = xna::Microsoft.Xna.Framework.Content.Pipeline.FbxImporter;
+    using MonoGameFbxImporter = monogame::Microsoft.Xna.Framework.Content.Pipeline.FbxImporter;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string asset = "dude.fbx";
+            var contentDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\Content"));
+            var intDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\obj"));
+            var outDir = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\bin"));
+            var absAssetPath = Path.Combine(contentDir, asset);
+
+            var xnaContext = new XnaImporterContext(Path.Combine(intDir, "xna"), Path.Combine(outDir, "xna"));
+            var xnaImporter = new XnaFbxImporter();
+            var xnaOutput = xnaImporter.Import(absAssetPath, xnaContext);
+
+            var mgContext = new MonoGameImporterContext(Path.Combine(intDir, "mg"), Path.Combine(outDir, "mg"));
+            var mgImporter = new MonoGameFbxImporter();
+            var mgOutput = mgImporter.Import(absAssetPath, mgContext);
+
+            // Compare objects here
+
+            Console.Write("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+    }
+}
