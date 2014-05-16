563fb3411b86b313fe3dd6ba6b69061fe4f37105
James Lupiani <lupiani@sickheadgames.com>
2014-05-16 11:37:53 -0500

    New project setup

---
+++ BuildContent.bat
@@ -0,0 +1,54 @@
+@rem Copyright 2014 James T. Lupiani
+@rem 
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem   http://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+
+@rem Batch script for building game content
+@rem Also happens to set up the MonoGame submodule if you haven't yet...
+
+@echo off
+@set MODE=Debug
+@set MONOGAME_ROOT=%~dp0%~1MonoGame
+@set GAME_PATH=%~dp0%~1ModelViewer
+
+@rem Do we have MonoGame set up yet?
+@if not exist "%MONOGAME_ROOT%" (
+    @echo Getting MonoGame...
+    git submodule update --init
+
+    @echo Getting MonoGame dependencies...
+    @pushd %MONOGAME_ROOT%
+    git submodule update --init --recursive
+    Protobuild.exe -generate Windows
+    @popd
+)
+
+@rem Make sure the MonoGame content builder is up to date first
+@echo Building MGCB...
+@call "%VS120COMNTOOLS%vsvars32.bat"
+@pushd %MONOGAME_ROOT%\Tools\MGCB
+msbuild /nologo /v:quiet MGCB.sln /p:Configuration=%MODE% /p:PlatformTarget=x64 /t:Clean,Build
+@popd
+
+@rem Build the actual assets and put them in the game's directory.
+@pushd Content
+@echo Building content...
+%MONOGAME_ROOT%\Tools\MGCB\bin\x64\%MODE%\MGCB.exe ^
+/rebuild ^
+/config:%MODE% ^
+/platform:Windows ^
+/intermediateDir:%GAME_PATH%\obj\%MODE%\Content ^
+/outputDir:%GAME_PATH%\Content ^
+/@:content.mgcb %1
+
+@echo Done.
+@pause
---
+++ ModelViewer/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+
+namespace ModelViewer
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var game = new ModelViewer(args))
+                game.Run();
+        }
+    }
+
+    public class ModelViewer : Game
+    {
+        private readonly string _fileName;
+        private readonly GraphicsDeviceManager _graphics;
+
+        private Model _model;
+        private Matrix[] _bones;
+
+        private Matrix _worldMatrix;
+        private Matrix _viewMatrix;
+        private Matrix _projMatrix;
+
+        public ModelViewer(string[] args)
+        {
+            this.Window.Title = "Model Viewer";
+
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            Content.RootDirectory = "Content";
+
+            if (args.Length > 0)
+            {
+                Window.Title += ": " + args[0];
+
+                _fileName = Path.Combine(
+                    Path.GetDirectoryName(args[0]),
+                    Path.GetFileNameWithoutExtension(args[0]));
+            }
+            else
+            {
+                _fileName = @"Dude\dude";
+            }
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _model = Content.Load<Model>(_fileName);
+            _bones = new Matrix[_model.Bones.Count];
+
+            var bestFit = new BoundingSphere();
+            foreach (var mesh in _model.Meshes)
+            {
+                if (bestFit.Contains(mesh.BoundingSphere) != ContainmentType.Contains)
+                    bestFit = BoundingSphere.CreateMerged(bestFit, mesh.BoundingSphere);
+            }
+
+            _worldMatrix = Matrix.Identity;
+            _viewMatrix = Matrix.CreateTranslation(bestFit.Center) * Matrix.CreateTranslation(0, 0, bestFit.Radius * 1.5f);
+            _projMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 0.1f, bestFit.Radius * 2.0f);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _model.CopyAbsoluteBoneTransformsTo(_bones);
+            foreach (var mesh in _model.Meshes)
+            {
+                foreach (IEffectMatrices em in mesh.Effects)
+                {
+                    em.World = _bones[mesh.ParentBone.Index];
+                    em.View = _viewMatrix;
+                    em.Projection = _projMatrix;
+                }
+
+                mesh.Draw();
+            }
+        }
+    }
+}
